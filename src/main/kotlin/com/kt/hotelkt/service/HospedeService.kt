a8b99ed0a6af8afd25d321e0dcdd9377b901900c
Leonardo Salmento <salmento_@hotmail.com>
2024-03-31 10:47:47 -0300

    Adicionando hospedeController e hospedeService

--- src/main/kotlin/com/kt/hotelkt/service/HospedeService.kt
+++ src/main/kotlin/com/kt/hotelkt/service/HospedeService.kt
@@ -1,12 +1,22 @@
 package com.kt.hotelkt.service
 
 import com.kt.hotelkt.model.Hospede
+import com.kt.hotelkt.model.Quarto
 import com.kt.hotelkt.repository.HospedeRepository
 import org.springframework.stereotype.Service
 
 @Service
 class HospedeService (private val repository: HospedeRepository){
-    fun buscarPorId(id: Long): Hospede{
-        return repository.getReferenceById(id)
+    fun buscarTodos(): List<Hospede> {
+        return  repository.findAll()
+    }
+
+    fun buscarPorId(id: Long): Hospede {
+        return  repository.getReferenceById(id)
+    }
+
+    fun cadastrar(hospede: Hospede): Hospede {
+        val newHospede = repository.save(hospede)
+        return newHospede
     }
 }
